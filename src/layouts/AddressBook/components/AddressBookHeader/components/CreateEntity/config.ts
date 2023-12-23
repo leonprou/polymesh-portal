0af8ab704d437adb3934415a9699cfd08f058656
Leon Prouger <leonprou@gmail.com>
2023-12-23 20:37:50 +0200

    Fixing CreateEntity config

--- src/layouts/AddressBook/components/AddressBookHeader/components/CreateEntity/config.ts
+++ src/layouts/AddressBook/components/AddressBookHeader/components/CreateEntity/config.ts
@@ -1,18 +1,19 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { VenueType } from '@polymeshassociation/polymesh-sdk/types';
+// import { VenueType } from '@polymeshassociation/polymesh-sdk/types';
+import { EAddressBookEntityTypes } from '../../../../types';
 import { ValidationMode } from 'react-hook-form';
 import * as yup from 'yup';
 
 export interface IFieldValues {
   description: string;
-  type: VenueType;
+  type: EAddressBookEntityTypes;
 }
 
 export const FORM_CONFIG = {
   mode: 'onTouched' as keyof ValidationMode,
   defaultValues: {
     description: '',
-    type: '' as VenueType,
+    type: '' as EAddressBookEntityTypes,
   },
   resolver: yupResolver(
     yup.object().shape({
